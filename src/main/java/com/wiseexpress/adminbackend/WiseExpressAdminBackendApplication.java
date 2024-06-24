b7064dae43ff418f4b05d22ba01c159d4dd7b3ec
Cjungwo <jwoo516@naver.com>
2024-06-24 18:19:47 +1000

    #1 - 프로젝트 초기화 및 초기 세팅
    Tech Environment
    - Intellij IDEA Ultimate 2024.1.4
    - Java 21
    - Gradle 8.8
    - Spring Boot 3.3.1
    - Spring Dependency Management 1.1.5
    
    Dependencies
    - Spring Boot Actuator
    - Spring Web
    - Spring Data JPA
    - H2 Database
    - MySQL Driver
    - Lombok
    - Spring Boot DevTools
    - Spring Configuration Processor

---
+++ src/main/java/com/wiseexpress/adminbackend/WiseExpressAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiseexpress.adminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseExpressAdminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiseExpressAdminBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiseexpress/adminbackend/WiseExpressAdminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiseexpress.adminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseExpressAdminBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
